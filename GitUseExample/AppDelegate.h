28224cb686c275b3dee8fad2db4905831e820d1a
Vincent Dulac <vdulac@Vincents-MacBook-Pro.local>
2014-04-01 17:39:46 -0400

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Vincent Dulac on 2014-04-01.
+//  Copyright (c) 2014 Vincent Dulac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Vincent Dulac on 2014-04-01.
+//  Copyright (c) 2014 Vincent Dulac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
